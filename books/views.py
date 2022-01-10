0548e35719020436ddc7c8aa185d8919dd17e223
ekiliar <eugene@natiki.com>
2022-01-10 15:41:06 -0500

    add static pages, added static simple css

--- books/views.py
+++ books/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """Index page, starting page"""
-    return render(request, "books/index.html")
+    return render(request, 'books/index.html')
 
 def books(request):
     """All books page"""
-    pass
+    return render(request, 'books/all-books.html')
 
 def book_detail(request):
     """Detailed book page"""
--- booksby/settings.py
+++ booksby/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

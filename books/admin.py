cfc6ffb518b2ea741068e2691b3d87474c4a723e
ekiliar <eugene@natiki.com>
2022-01-12 09:30:31 -0500

    updated models and migrations

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Book, Genre, LinkType, Link
+
+admin.site.register(Person)
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Link)
+admin.site.register(LinkType)
--- books/models.py
+++ books/models.py
@@ -1,10 +1,8 @@
-from io import open_code
 import uuid
 
 from django.db import models
-from django.db.models.deletion import CASCADE, SET_DEFAULT, SET_NULL
+from django.db.models.deletion import CASCADE, SET_NULL
 from django.db.models.fields import TextField
-from django.db.models.fields.related import ForeignKey, OneToOneField
 from django.utils.translation import gettext as _
 from django.utils.text import slugify
 from django.urls import reverse
@@ -12,8 +10,8 @@ from django.urls import reverse
 
 class Person(models.Model):
     uuid = models.UUIDField(_("person_id"), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField
-    description = models.TextField
+    name = models.CharField(_('person_name'), max_length=100, default='')
+    description = models.TextField(_('person_description'))
 
 
 class Genre(models.Model):
@@ -24,20 +22,17 @@ class Genre(models.Model):
 
 class Book(models.Model):
     uuid = models.UUIDField(_("book_id"), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    title = models.CharField(_('title'), max_length=70, blank=True, default='')
-    description = models.TextField(_('text'))
+    title = models.CharField(_('title'), max_length=100, blank=True, default='')
+    description = models.TextField(_('book_description'))
     date = models.DateTimeField(_('created_at'), auto_now_add=False)
-    authors = models.ManyToManyField(Person)
-    narrators = models.ManyToManyField(Person)
-    translators = models.ManyToManyField(Person)
-    slug = models.SlugField(_('slug'), unique=True, db_index=True, default='', null=False)
-    cover_url = models.URLField(_(''))
-    genre = models.ManyToManyField(Genre)
+    authors = models.ManyToManyField(Person, related_name='authors')
+    narrators = models.ManyToManyField(Person, related_name='narrators')
+    translators = models.ManyToManyField(Person, related_name='translators')
+    slug = models.SlugField(_('slug'), unique=True, db_index=True, null=False)
+    cover_image_name = models.CharField(max_length=50)
+    genre = models.ManyToManyField(Genre, related_name='genre')
     promoted = models.BooleanField(_(''), default=False)
-    annotation = TextField(_('annotation'))
-
-    # def get_absolute_url(self):
-    #     return reverse("book_detail", args=[self.uuid])
+    annotation = TextField(_('book_annotation'))
     
     def __str__(self) -> str:
         return f'{self.authors} - {self.title}'
@@ -47,20 +42,18 @@ class Book(models.Model):
         super().save(*args, **kwargs)
 
 
-
 class LinkType(models.Model):
-    uuid = models.UUIDField(_("type_id"), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField
-    caption = models.CharField
-    icon_url = models.URLField
+    name = models.CharField(_('link_type'), max_length=70, blank=True, default='')
+    caption = models.CharField(_('link_caption'), max_length=100, blank=True, default='')
+    icon_name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return f'{self.name}'
 
 class Link(models.Model):
     uuid = models.UUIDField(_("link_id"), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    url = models.URLField(_("url"), max_length=200)
-    url_type = models.ForeignKey(LinkType, on_delete=SET_NULL)
+    url = models.URLField(_("url"), max_length=300)
+    url_type = models.ForeignKey(LinkType, null=True, on_delete=SET_NULL)
     book = models.ForeignKey(Book, on_delete=CASCADE)
 
     def __str__(self) -> str:

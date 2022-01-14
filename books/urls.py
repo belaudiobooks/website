5d88d11a44edc33e4acbd25a5b667bdb34802f8b
ekiliar <eugene@natiki.com>
2022-01-14 17:44:38 -0500

    strated with designs - navbar is almost ready

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books', views.books, name='books-page'),
+    path('books', views.books, name='books'),
     path('books/<slug:slug>', views.book_detail, name='book-detail-page'),
     path('person/<slug:slug>', views.person_detail, name='person-detail-page'),
     path('search', views.search, name='search')

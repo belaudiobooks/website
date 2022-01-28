32eb0e05dbe20cc0cddf7165eaaf432421f0bfba
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-01-27 22:41:21 -0800

    Add command to push books data to prod.

---
+++ books/management/commands/push_data_to_prod.py
@@ -0,0 +1,53 @@
+'''
+Runs server using tmp sqlite3 database and loads data from dump object.
+'''
+
+from collections import defaultdict
+import subprocess
+import os
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.conf import settings
+import django
+
+
+class Command(BaseCommand):
+    help = 'Pushes books data and images to production.'
+
+    def handle(self):
+        try:
+            django.db.connection.ensure_connection()
+        except django.db.utils.OperationalError:
+            print(
+                'Cannot connect to DB. Make sure you are running\n' +
+                'cloud_sql_proxy -instances="audiobooksbysite:europe-west1:' +
+                'audiobooks-prod"=tcp:5432')
+            return
+        db_path = settings.DATABASES['default']['NAME']
+        data_dir = os.environ.get('BOOKS_DATA_DIR', None)
+        if data_dir is None:
+            print('Must provide BOOKS_DATA_DIR variable that points to ' +
+                  'location of audiobooks/data repo on local disk.')
+            return
+        print(f'Using database {db_path}')
+        call_command('makemigrations')
+        call_command('migrate', 'books', 'zero')
+        call_command('migrate', 'books')
+        for dir in ['covers', 'photos', 'icons']:
+            full_dir = os.path.join(data_dir, dir)
+            print(f'Pushing {dir}')
+            subprocess.run(
+                ['gsutil', '-m', 'rsync', full_dir, f'gs://books_media/{dir}'],
+                check=True)
+        with open(os.path.join(data_dir, 'data.json'), encoding='utf8') as f:
+            deserialized = django.core.serializers.deserialize('json', f)
+            obj_dict = defaultdict(list)
+            # organize by model class
+            for item in deserialized:
+                obj = item.object
+                obj_dict[obj.__class__].append(obj)
+
+            for cls, objs in obj_dict.items():
+                print(f'Adding models {cls}')
+                cls.objects.bulk_create(objs)
+        print('Completed!')
--- books/management/commands/runserver_with_tmp_db.py
+++ books/management/commands/runserver_with_tmp_db.py
@@ -1,6 +1,6 @@
-"""
+'''
 Runs server using tmp sqlite3 database and loads data from dump object.
-"""
+'''
 
 import os
 from django.core.management.base import BaseCommand
@@ -12,10 +12,10 @@ class Command(BaseCommand):
     help = 'Initializes DB in tmp file with data provided in dump and runs server on that db.'
 
     def handle(self, *args, **options):
-        db_path = settings.DATABASES["default"]["NAME"]
-        print(f"Using database {db_path}")
-        call_command("makemigrations")
-        call_command("migrate")
-        data_dir = os.environ["BOOKS_DATA_DIR"]
-        call_command("loaddata", os.path.join(data_dir, "data.json"))
-        call_command("runserver")
+        db_path = settings.DATABASES['default']['NAME']
+        print(f'Using database {db_path}')
+        call_command('makemigrations')
+        call_command('migrate')
+        data_dir = os.environ['BOOKS_DATA_DIR']
+        call_command('loaddata', os.path.join(data_dir, 'data.json'))
+        call_command('runserver')

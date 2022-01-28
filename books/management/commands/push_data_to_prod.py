dcd07fcdc4ce753161cdb1ff5bc2203ca2250689
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-01-27 23:01:56 -0800

    Update push_data_to_prod script to use loaddata command.

--- books/management/commands/push_data_to_prod.py
+++ books/management/commands/push_data_to_prod.py
@@ -5,6 +5,7 @@ Runs server using tmp sqlite3 database and loads data from dump object.
 from collections import defaultdict
 import subprocess
 import os
+from tabnanny import verbose
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.conf import settings
@@ -14,7 +15,7 @@ import django
 class Command(BaseCommand):
     help = 'Pushes books data and images to production.'
 
-    def handle(self):
+    def handle(self, *args, **options):
         try:
             django.db.connection.ensure_connection()
         except django.db.utils.OperationalError:
@@ -39,15 +40,23 @@ class Command(BaseCommand):
             subprocess.run(
                 ['gsutil', '-m', 'rsync', full_dir, f'gs://books_media/{dir}'],
                 check=True)
-        with open(os.path.join(data_dir, 'data.json'), encoding='utf8') as f:
-            deserialized = django.core.serializers.deserialize('json', f)
-            obj_dict = defaultdict(list)
-            # organize by model class
-            for item in deserialized:
-                obj = item.object
-                obj_dict[obj.__class__].append(obj)
 
-            for cls, objs in obj_dict.items():
-                print(f'Adding models {cls}')
-                cls.objects.bulk_create(objs)
+        print('Uploading data to DB. It is going to be slow...')
+        call_command('loaddata',
+                     os.path.join(data_dir, 'data.json'),
+                     verbosity=3)
+        # Fast but non-working way of adding data as relations between models get lost
+        # for some reason.
+        # https://stackoverflow.com/questions/19306807/django-fixture-loading-very-slow
+        # with open(os.path.join(data_dir, 'data.json'), encoding='utf8') as f:
+        #     deserialized = django.core.serializers.deserialize('json', f)
+        #     obj_dict = defaultdict(list)
+        #     # organize by model class
+        #     for item in deserialized:
+        #         obj = item.object
+        #         obj_dict[obj.__class__].append(obj)
+
+        #     for cls, objs in obj_dict.items():
+        #         print(f'Adding models {cls}')
+        #         cls.objects.bulk_create(objs)
         print('Completed!')

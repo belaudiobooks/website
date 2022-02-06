37d5fef9f3eeda7ade7b80ccd722c16466659774
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-02-05 18:55:13 -0800

    Add admin model filters for missing data.
    These filters will help to identify models (books, persons) that have missing data and should be fixed.

--- books/admin.py
+++ books/admin.py
@@ -1,12 +1,55 @@
 from django.contrib import admin
 from django.contrib.admin.decorators import display
+from django.db.models import Count
 
 from .models import Person, Book, Tag, LinkType, Link
 
 
+class IncompleteBookListFilter(admin.SimpleListFilter):
+    '''Filter that shows books that have incomplete data like missing description or cover.'''
+    title = 'incomplete data'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'incomplete_reason'
+
+    def lookups(self, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        reasons = {
+            'no_description': 'Missing description',
+            'no_cover': 'Missing cover',
+            'no_duration': 'Missing duration',
+            'no_narrators': 'Missing narrators',
+            'no_tags': 'Missing tags',
+        }
+        return [
+            (key,
+             f'{value} ({self._get_books_for_reason(queryset, key).count()})')
+            for key, value in reasons.items()
+        ]
+
+    def queryset(self, request, queryset):
+        return self._get_books_for_reason(queryset, self.value())
+
+    def _get_books_for_reason(self, queryset, reason):
+        if reason is None:
+            return queryset
+        if reason == 'no_description':
+            return queryset.filter(description__exact='')
+        if reason == 'no_cover':
+            return queryset.filter(cover_image__exact='')
+        if reason == 'no_duration':
+            return queryset.filter(duration_sec__exact=None)
+        if reason == 'no_narrators':
+            return queryset.annotate(num_narrators=Count('narrators')).filter(
+                num_narrators=0)
+        if reason == 'no_tags':
+            return queryset.annotate(num_tags=Count('tag')).filter(num_tags=0)
+        raise ValueError(f'unknown incomplete_reason: {reason}')
+
+
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
-    list_filter = ('authors', 'title', 'promoted')
+    list_filter = (IncompleteBookListFilter, 'authors', 'title', 'promoted')
     list_display = ('title', 'get_book_authors', 'promoted')
 
     @display(description='authors')
@@ -22,9 +65,41 @@ class LinkAdmin(admin.ModelAdmin):
         return obj.book
 
 
+class IncompletePersonListFilter(admin.SimpleListFilter):
+    '''Filter that shows people that have incomplete data like missing description or photo.'''
+    title = 'incomplete data'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'incomplete_reason'
+
+    def lookups(self, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        reasons = {
+            'no_description': 'Missing description',
+            'no_photo': 'Missing photo',
+        }
+        return [
+            (key,
+             f'{value} ({self._get_people_for_reason(queryset, key).count()})')
+            for key, value in reasons.items()
+        ]
+
+    def queryset(self, request, queryset):
+        return self._get_people_for_reason(queryset, self.value())
+
+    def _get_people_for_reason(self, queryset, reason):
+        if reason is None:
+            return queryset
+        if reason == 'no_description':
+            return queryset.filter(description__exact='')
+        if reason == 'no_photo':
+            return queryset.filter(photo__exact='')
+        raise ValueError(f'unknown incomplete_reason: {reason}')
+
+
 class PersonAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
-    list_filter = ('name', )
+    list_filter = (IncompletePersonListFilter, )
 
 
 admin.site.register(Person, PersonAdmin)

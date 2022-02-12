1753b42db4e7e93639667e72dbea0aea5c050d51
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-02-12 15:17:40 -0800

    Add comments to models.

--- books/models.py
+++ books/models.py
@@ -18,6 +18,11 @@ def _get_image_name(folder: str, instance: Union['Person', 'Book'], filename: st
 
 
 class Person(models.Model):
+    '''
+    Model representing single person. Person can perform multiple roles, like being an
+    author, translator or narrator. Those roles aren't set on person, but can be derived
+    from fields in other models, like Book.authors.
+    '''
     uuid = models.UUIDField(_('Person Id'), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(_('Person Name'), max_length=100, default='')
     description = models.TextField(_('Person Description'), blank=True)
@@ -34,6 +39,10 @@ class Person(models.Model):
 
 
 class Tag(models.Model):
+    '''
+    Tag is a group of books that have some common trait. For example different genres are
+    examples of tags. Each book can and should have one or more tags.
+    '''
     name = models.CharField(_('Tag'), max_length=50, default='')
 
     def __str__(self) -> str:
@@ -41,6 +50,11 @@ class Tag(models.Model):
 
 
 class Book(models.Model):
+    '''
+    Book model represents a single book... It can have multiple authors and or translators. Single
+    books can have multiple narrations if it was narrated by different groups of people. Each book
+    should have at least one Narration.
+    '''
     uuid = models.UUIDField(_('Book ID'), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(_('Book Title'), max_length=100, blank=True, default='')
     description = models.TextField(_('Book Description'))
@@ -71,6 +85,11 @@ class Book(models.Model):
 
 
 class Narration(models.Model):
+    '''
+    Narration represents particular narration of a book. It's represented through a separate model so
+    that we can visually separate narrations in UI. As different narration fo the same books have different
+    narrators and set of links.
+    '''
     uuid = models.UUIDField(_('Narration ID'), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     narrators = models.ManyToManyField(Person, related_name='narrators', blank=True)
     book = models.ForeignKey(Book, related_name='narration', blank=True, null=True, on_delete=SET_NULL)
@@ -82,8 +101,12 @@ class Narration(models.Model):
         )
 
 class LinkType(models.Model):
+    '''
+    LinkType represents a particular source in internet where audibooks are hosted. Examples are
+    Google podcasts, apple podcasts, Knizhny Voz, LitRes.
+    '''
     name = models.CharField(_('Link Type Name'), max_length=70, blank=True, default='')
-    caption = models.CharField(_('Link =Caption'), max_length=100, blank=True, default='')
+    caption = models.CharField(_('Link Caption'), max_length=100, blank=True, default='')
     icon = models.ImageField(upload_to='icons', blank=True, null=True)
 
     def __str__(self) -> str:
@@ -91,6 +114,10 @@ class LinkType(models.Model):
 
 
 class Link(models.Model):
+    '''
+    Link represents a URL link that points to page where users can find and listen to an audiobook.
+    For example it can be link to particular Google Podcast or audiobook page on LitRes.
+    '''
     uuid = models.UUIDField(_('Link Id'), primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     url = models.URLField(_('URL'), max_length=300)
     url_type = models.ForeignKey(LinkType, related_name='link_type', null=True, on_delete=SET_NULL)

e3356a692bde4c1b4249f90cebd808ef2a2247c2
ekiliar <eugene@natiki.com>
2022-02-14 11:00:58 -0500

    Added pluralization filter for books count #23

---
+++ books/templatetags/books_extras.py
@@ -0,0 +1,21 @@
+"""Pluralization filter for books count in templates"""
+
+from atexit import register
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def by_plural(value, variants):
+    variants = variants.split(",")
+    value = abs(int(value))
+
+    if value % 10 == 1 and value % 100 != 11:
+        variant = 0
+    elif value % 10 >= 2 and value % 10 <= 4 and (value % 100 < 10 or value % 100 >= 20):
+        variant = 1
+    else:
+        variant = 2
+    
+    return variants[variant]

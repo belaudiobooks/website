9a1f7ba36e52bee334122b9426c245d60278db3d
ekiliar <eugene@natiki.com>
2022-02-20 17:38:39 -0500

    implemented and fixed #36

--- books/templatetags/books_extras.py
+++ books/templatetags/books_extras.py
@@ -1,15 +1,16 @@
 """Pluralization filter for books count in templates"""
 
 from atexit import register
-from nis import match
 from django import template
+
+import re
 
 register = template.Library()
 
 
 @register.filter
 def by_plural(value, variants):
-    variants = variants.split(",")
+    variants = variants.split(',')
     value = abs(int(value))
 
     if value % 10 == 1 and value % 100 != 11:
@@ -23,7 +24,7 @@ def by_plural(value, variants):
 
 @register.filter
 def gender(value, variants):
-    variants = variants.split(",")
+    variants = variants.split(',')
 
     if value and value=='FEMALE':
         variant = 0
@@ -31,3 +32,28 @@ def gender(value, variants):
         variant = 1
     
     return variants[variant]
+
+@register.filter
+def duration(value):
+    day, seconds = value.days, value.seconds
+   
+    hrs = seconds // 3600
+    mins = (seconds % 3600) // 60
+    secs = seconds % 60
+    
+    #check if more than 24 horus and assign correct spelling for values
+    hours = by_plural(hrs,'гадзіна,гадзіны,гадін')
+    minutes = by_plural(mins,'мінута,мінуты,мінут')
+    seconds = by_plural(secs,'сякунда,сякунды,сякунд')
+
+    if day:
+        days = by_plural(day,'дзень,дня,дней')
+        #return values with spelling in Belarussian if more than 24 hours
+        return f'{day} {days} {hrs} {hours} {mins} {minutes} {secs} {seconds}'
+    
+    #return values with spelling in Belarussian
+    return f'{hrs} {hours} {mins} {minutes} {secs} {seconds}'
+
+
+        
+    

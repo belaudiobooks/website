48bffb35a667083c7d25ebefda021d2a6c999ed0
ekiliar <eugene@natiki.com>
2022-02-25 16:24:33 -0500

    fixed #41

--- books/templatetags/books_extras.py
+++ books/templatetags/books_extras.py
@@ -50,3 +50,36 @@ def duration(value):
         return f'{hrs} {hours} {mins} {minutes}'
     
     return f'{mins} {minutes}'
+
+@register.filter
+def colors(value):
+    """getting quotient of the ASCII code of the first sympol of uuid, 
+    where 0 is 0, 1-3 are 1, 4-6 are 2, 7-9 are 3, a-c are 4 and d-f are 5
+    """
+    first = str(value)[0]
+    if first.isdigit():
+        if int(first) == 0:
+            key = 0
+        elif int(first) in range(1, 4):
+            key = 1
+        elif int(first) in range(4, 7):
+            key = 1
+        elif int(first) in range(7, 10):
+            key = 2  
+    else:
+        if first in ['a','b','c']:
+            key = 3
+        else: key = 4
+
+    colors = {
+        0:'/static/cover_templates/cover_templates_blue.jpeg',
+        1:'/static/cover_templates/cover_templates_green.jpeg',
+        2:'/static/cover_templates/cover_templates_grey.jpeg',
+        3:'/static/cover_templates/cover_templates_purple.jpeg',
+        4:'/static/cover_templates/cover_templates_red.jpeg',
+        5:'/static/cover_templates/cover_templates_yellow.jpeg'
+    }
+
+    result = colors.setdefault(key,0)
+
+    return result

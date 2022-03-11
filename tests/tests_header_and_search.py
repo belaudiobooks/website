d9b70e70fe732ed30479a0eb6240e292b0289e80
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-03-10 23:35:33 -0800

    Fix github action config.

--- tests/tests_header_and_search.py
+++ tests/tests_header_and_search.py
@@ -2,7 +2,6 @@ import time
 from books import models
 from tests.webdriver_test_case import WebdriverTestCase
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 

eaaf43aa7ba2ab787578c605a10f4b9fc3e107ad
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-03-29 22:18:37 -0700

    Refactor and improve template filters.

--- books/templatetags/books_extras.py
+++ books/templatetags/books_extras.py
@@ -1,86 +1,75 @@
-"""Pluralization filter for books count in templates"""
+'''Various helper template filters for books.'''
 
 from atexit import register
 from django import template
 
-import re
+from books import models
 
 register = template.Library()
 
 
 @register.filter
 def by_plural(value, variants):
-    variants = variants.split(',')
+    '''
+    Chooses correct plural version of a wordt given number.
+    variants is a comma-separated list of words.
+    '''
     value = abs(int(value))
 
     if value % 10 == 1 and value % 100 != 11:
         variant = 0
-    elif value % 10 >= 2 and value % 10 <= 4 and (value % 100 < 10 or value % 100 >= 20):
+    elif value % 10 >= 2 and value % 10 <= 4 and (value % 100 < 10
+                                                  or value % 100 >= 20):
         variant = 1
     else:
         variant = 2
-    
-    return variants[variant]
+
+    return str(value) + ' ' + variants.split(',')[variant]
+
 
 @register.filter
-def gender(value, variants):
-    variants = variants.split(',')
-
-    if value and value=='FEMALE':
+def gender(person: models.Person, variants: str):
+    '''Choses correct ending of a gender-full word given comma-separated endings as "variants".'''
+    if person.gender == 'FEMALE':
         variant = 0
-    else: 
+    else:
         variant = 1
-    
-    return variants[variant]
+
+    return variants.split(',')[variant]
+
 
 @register.filter
-def duration(value):
-    day, seconds = value.days, value.seconds
-    
+def duration(book: models.Book):
+    '''Formats book duration using "36 hours 12 minutes" format.'''
+    day = book.duration_sec.days
+    seconds = book.duration_sec.seconds
+
     hrs = seconds // 3600
     mins = (seconds % 3600) // 60
 
-    minutes = by_plural(mins,'хвіліна,хвіліны,хвілін')
+    minutes = by_plural(mins, 'хвіліна,хвіліны,хвілін')
 
     if day:
         hrs = hrs + (day * 24)
-    
+
     if hrs:
-        hours = by_plural(hrs,'гадзіна,гадзіны,гадзін')
-        return f'{hrs} {hours} {mins} {minutes}'
-    
-    return f'{mins} {minutes}'
+        hours = by_plural(hrs, 'гадзіна,гадзіны,гадзін')
+        return f'{hours} {minutes}'
+
+    return minutes
+
+
+COVER_PATTERNS = [
+    '/static/cover_templates/cover_templates_blue.jpeg',
+    '/static/cover_templates/cover_templates_green.jpeg',
+    '/static/cover_templates/cover_templates_grey.jpeg',
+    '/static/cover_templates/cover_templates_purple.jpeg',
+    '/static/cover_templates/cover_templates_red.jpeg',
+    '/static/cover_templates/cover_templates_yellow.jpeg',
+]
+
 
 @register.filter
-def colors(value):
-    """char code of the first sympol of uuid, 
-    where 0 is 0, 1-3 are 1, 4-6 are 2, 7-9 are 3, a-c are 4 and d-f are 5
-    """
-    first = str(value)[1]
-    if first.isdigit():
-        if int(first) == 0:
-            key = 0
-        elif int(first) in range(1, 4):
-            key = 1
-        elif int(first) in range(4, 7):
-            key = 1
-        elif int(first) in range(7, 10):
-            key = 2  
-    else:
-        if first in ['a','b','c']:
-            key = 3
-        else: key = 4
-
-    colors = {
-        0:'/static/cover_templates/cover_templates_blue.jpeg',
-        1:'/static/cover_templates/cover_templates_green.jpeg',
-        2:'/static/cover_templates/cover_templates_grey.jpeg',
-        3:'/static/cover_templates/cover_templates_purple.jpeg',
-        4:'/static/cover_templates/cover_templates_red.jpeg',
-        5:'/static/cover_templates/cover_templates_yellow.jpeg'
-    }
-
-    #choose the color, if key not exists choose the first one
-    result = colors.setdefault(key,0)
-
-    return result
+def colors(book: models.Book):
+    '''Returns random cover template for a book that has no cover.'''
+    return COVER_PATTERNS[book.uuid.int % len(COVER_PATTERNS)]

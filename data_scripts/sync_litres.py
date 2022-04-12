d260c49ad44aed76a1b229b41b2db2f746be7ef2
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-04-11 22:52:23 -0700

    Improve litres script.

--- data_scripts/sync_litres.py
+++ data_scripts/sync_litres.py
@@ -19,6 +19,8 @@ REPLACEMENTS = {
     'Владимир Лисовский': 'Уладзімір Лісоўскі',
     'Ольгерд Бахаревич': 'Альгерд Бахарэвіч',
     'Сборник': 'Народныя Творы',
+    'Саша Филипенко': 'Саша Філіпенка',
+    'Анатолий Белый': 'Анатоль Белы',
 }
 
 
@@ -50,25 +52,27 @@ def _sync_book(data: books.BooksData, url: str) -> None:
     print(f'Processing {title}')
     author = _maybe_replace(
         soup.select_one('.biblio_book_author span[itemprop=name]').string)
-    narrator = None
+    narrators = []
     details = soup.select('.biblio_book_info_detailed li')
     duration = datetime.timedelta()
     for detail in details:
         detail_name = detail.select_one('strong')
         name = '' if detail_name is None else detail_name.string
         if name == 'Чтец:':
-            narrator = _maybe_replace(
-                detail.select_one('.biblio_info_detailed__link').string)
+            narrators.append(
+                _maybe_replace(
+                    detail.select_one('.biblio_info_detailed__link').string))
         elif name == 'Длительность:':
             detail_name.string = ''
             duration_str = '\n'.join(detail.stripped_strings)
             parts = LITRES_DURATION_FORMAT.match(duration_str)
+            if parts is None:
+                parts = LITRES_DURATION_FORMAT.match('0 ч. ' + duration_str)
             assert parts, f'Duration regex did not match. {duration_str}'
             time_parts = {}
             for name, param in parts.groupdict().items():
                 time_parts[name] = int(param)
             duration = datetime.timedelta(**time_parts)
-    assert narrator, 'Did not find narrator'
     description = '\n'.join(
         soup.select_one('.biblio_book_descr_publishers').stripped_strings)
     cover = soup.select_one(
@@ -78,7 +82,7 @@ def _sync_book(data: books.BooksData, url: str) -> None:
                                          title=title,
                                          description=description,
                                          authors=[author],
-                                         narrators=[narrator],
+                                         narrators=narrators,
                                          translators=[],
                                          cover_url=cover,
                                          duration_sec=int(
@@ -90,25 +94,15 @@ def _sync_book(data: books.BooksData, url: str) -> None:
 
 
 EXTRA_BOOKS = [
-    'https://www.litres.ru/maksim-zhbankou/slomo-hatnyaya-krytyka-kulturnaga-dyzaynu-66596742/',
-    'https://www.litres.ru/eva-vezhnavec/shlyah-drobnay-svolachy-67123155/',
+    'https://www.litres.ru/sasha-filipenko-10901725/zamysly-40264023/',
+    'https://www.litres.ru/sasha-filipenko-10901725/krasnyy-krest-28947141/',
+    'https://www.litres.ru/sasha-filipenko-10901725/novaya-volna-rasskazy-57239181/',
+    'https://www.litres.ru/sasha-filipenko-10901725/vozvraschenie-v-ostrog-50389654/',
 ]
-
-
-def _get_belarusian_audiobooks_links() -> List[str]:
-    search_url = 'https://www.litres.ru/novie/audioknigi/?lang=4'
-    resp = requests.get(search_url)
-    if resp.status_code != 200:
-        raise ValueError(f'URL {search_url} returned {resp.status_code}')
-    soup = bs4.BeautifulSoup(resp.text, 'html.parser')
-    return [
-        'https://www.litres.ru' + a['href']
-        for a in soup.select('.books_box .cover-image-wrapper a')
-    ]
 
 
 def run(data: books.BooksData):
     '''Sync function. See module description for details.'''
-    urls = _get_belarusian_audiobooks_links() + EXTRA_BOOKS
+    urls = EXTRA_BOOKS
     for url in urls:
         _sync_book(data, url)

012d24bd3b96f2f76bfbcd912dca7d2d91d83f95
Mikita Belahlazau <nikelandjelo@gmail.com>
2022-07-29 00:40:29 -0700

    Add 301 redirect from www to non-www.

---
+++ books/middleware.py
@@ -0,0 +1,15 @@
+from django.http import HttpRequest, HttpResponsePermanentRedirect
+
+
+class WwwRedirectMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest):
+        host = request.get_host().partition(":")[0]
+        if host.startswith('www.'):
+            return HttpResponsePermanentRedirect(request.get_raw_uri().replace(
+                '://www.', '://'))
+        else:
+            return self.get_response(request)

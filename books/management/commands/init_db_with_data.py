6e73a08ebf29cadb8786fccb4dbcb83ea5f4bede
Mikita Belahlazau <nikelandjelo@gmail.com>
2023-01-14 17:27:49 -0800

    Enable auto-reload for runserver_with_tmp_db script.

--- books/management/commands/init_db_with_data.py
+++ books/management/commands/init_db_with_data.py
@@ -2,6 +2,7 @@
 See Command desription.
 '''
 
+import os
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.conf import settings
@@ -12,12 +13,35 @@ class Command(BaseCommand):
 
     help = 'Initializes default DB using data from "data" submodule.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--skip-if-exists',
+            action='store_true',
+            help='Does not creates database if it already exists.',
+        )
+        parser.add_argument(
+            '--create-superuser',
+            help=
+            'Creates superuser with using given email. Password is read from DJANGO_SUPERUSER_PASSWORD variable',
+        )
+
     def handle(self, *args, **options):
         db_path = settings.DATABASES['default']['NAME']
-        print(f'Using database {db_path}')
+        if options['skip_if_exists'] and os.path.exists(db_path):
+            print(
+                f'Not creating database {db_path} as it already exists and --skip-if-exists is enabled.'
+            )
+            return
+        print(f'Creating database {db_path}')
         call_command('makemigrations')
         call_command('migrate', 'books', 'zero')
         call_command('migrate', 'user', 'zero')
         call_command('migrate')
         call_command('loaddata', 'data/data.json')
+        if 'create_superuser' in options:
+            superuser_pass = os.environ.get('DJANGO_SUPERUSER_PASSWORD', None)
+            assert superuser_pass, 'DJANGO_SUPERUSER_PASSWORD must be set when using --create-superuser option'
+            email = options['create_superuser']
+            call_command('createsuperuser', '--no-input', email=email)
+            print(f'Create superuser {email} with pass {superuser_pass}')
         print('Completed!')
--- books/management/commands/runserver_with_tmp_db.py
+++ books/management/commands/runserver_with_tmp_db.py
@@ -2,10 +2,8 @@
 Runs server using tmp sqlite3 database and loads data from dump object.
 '''
 
-import os
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -14,18 +12,7 @@ class Command(BaseCommand):
     help = 'Initializes DB in tmp file with data provided in dump and runs server on that db.'
 
     def handle(self, *args, **options):
-        call_command('init_db_with_data')
-
-        superuser_pass = os.environ.get('DJANGO_SUPERUSER_PASSWORD', None)
-        if superuser_pass is None:
-            print(
-                'Superuser not created. Pass DJANGO_SUPERUSER_PASSWORD=12345 to create superuser'
-            )
-        else:
-            call_command('createsuperuser',
-                         '--no-input',
-                         email='admin@example.com')
-            print(
-                f'Create superuser admin@example.com with pass {superuser_pass}'
-            )
-        call_command('runserver', '--noreload')
+        call_command('init_db_with_data',
+                     '--skip-if-exists',
+                     create_superuser='admin@example.com')
+        call_command('runserver')

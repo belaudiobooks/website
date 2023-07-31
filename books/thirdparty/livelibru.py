00e6c487aa720f7a43241b8325904eb462883755
Mikita Belahlazau <nikelandjelo@gmail.com>
2023-07-30 23:25:35 -0700

    Implement LiveLib link search on Book edit admin page. Issue #63.
    As part of this change updated API that returns LiveLib links to not filter out links with no reviews and instead return reviews count in response. All links are displayed and admin can decide which one to use.

--- books/thirdparty/livelibru.py
+++ books/thirdparty/livelibru.py
@@ -13,12 +13,18 @@ import json
 import math
 
 REQUIRED_HEADERS = {
-    "Host": "www.livelib.ru",
-    "Accept-Language": "en-US;q=1.0, ru-US;q=0.9, be-US;q=0.8",
-    "Accept": "*/*",
-    "Connection": "keep-alive",
-    "Accept-Encoding": "gzip;q=1.0, compress;q=0.5",
-    "User-Agent": "LiveLib/3.24.5 (ru.livelib.LiveLib; build:3.24.5; iOS 16.5.1) Alamofire/3.24.5"
+    "Host":
+    "www.livelib.ru",
+    "Accept-Language":
+    "en-US;q=1.0, ru-US;q=0.9, be-US;q=0.8",
+    "Accept":
+    "*/*",
+    "Connection":
+    "keep-alive",
+    "Accept-Encoding":
+    "gzip;q=1.0, compress;q=0.5",
+    "User-Agent":
+    "LiveLib/3.24.5 (ru.livelib.LiveLib; build:3.24.5; iOS 16.5.1) Alamofire/3.24.5"
 }
 
 SEARCH_PAGE_SIZE = 100
@@ -31,11 +37,11 @@ def _generate_session_guid():
         params={
             'andyll': 'and7mpp4ss',
             'fields': 'guid'
-        }
-    )
+        })
     if init_session_response.status_code > 299:
-        raise RuntimeError(f"Failed to start new livelib session, {init_session_response.text}, " +
-                           f"response status: {init_session_response.status_code}")
+        raise RuntimeError(
+            f"Failed to start new livelib session, {init_session_response.text}, "
+            + f"response status: {init_session_response.status_code}")
     guid = json.loads(init_session_response.text).get("data").get("guid")
     return guid
 
@@ -46,20 +52,22 @@ def _get_another_page(query_text, session_token, start_from=1):
         headers=REQUIRED_HEADERS,
         params={
             'andyll': 'and7mpp4ss',
-            'fields': 'author_id,author_name,avg_mark,id,count_reviews,is_work,name,pic_200,share_url(share_url),user_book_partial(book_read,rating)',
+            'fields':
+            'author_id,author_name,avg_mark,id,count_reviews,is_work,name,pic_200,share_url(share_url),user_book_partial(book_read,rating)',
             'count': SEARCH_PAGE_SIZE,
             'start': start_from,
             'q': query_text.lower(),
             'app_session_guid': session_token
-        }
-    )
+        })
     if search_response.status_code > 299:
-        raise RuntimeError(f"Failed to find book in livelib by title, {search_response.text}, " +
-                           f"response status: {search_response.status_code}")
+        raise RuntimeError(
+            f"Failed to find book in livelib by title, {search_response.text}, "
+            + f"response status: {search_response.status_code}")
     body = json.loads(search_response.text)
     if body.get('status').get('code') > 299:
-        raise RuntimeError(f"Failed to find book in livelib by title, {body}, " +
-                           f"response status: {body.get('status').get('code')}")
+        raise RuntimeError(
+            f"Failed to find book in livelib by title, {body}, " +
+            f"response status: {body.get('status').get('code')}")
     return body
 
 
@@ -70,7 +78,9 @@ def _find_books_by_title(query_text):
     if pages > 1:
         results = list(first_page.get('data'))
         for i in range(1, pages):
-            page = _get_another_page(query_text, session_token, start_from=i + 1)
+            page = _get_another_page(query_text,
+                                     session_token,
+                                     start_from=i + 1)
             results = results + page.get('data')
         return results
     else:
@@ -79,25 +89,24 @@ def _find_books_by_title(query_text):
 
 def search_books_with_reviews(search_request_text):
     """
-    Method returns result of search books having reviews on livelib.ru
+    Method returns result of search books on livelib.ru
     Example:
     [{
         "name": "Вершы і паэмы (сборник)",
         "author_name": "Янка Купала",
         "cover_image": "https://s1.livelib.ru/boocover/1001598084/200/50b7/boocover.jpg",
-        "url": "https://www.livelib.ru/book/1001598084-vershy-i-paemy-sbornik-yanka-kupala"
+        "url": "https://www.livelib.ru/book/1001598084-vershy-i-paemy-sbornik-yanka-kupala",
+        "reviews": 1,
     }]
     """
     results = []
     books = _find_books_by_title(search_request_text)
     for book in books:
-        if book.get('count_reviews') and book.get('count_reviews') > 0:
-            results.append(
-                {
-                    'name': book.get('name'),
-                    'author_name': book.get('author_name'),
-                    'cover_image': book.get('pic_200'),
-                    'url': book.get('share_url').get('share_url')
-                }
-            )
+        results.append({
+            'name': book.get('name'),
+            'author_name': book.get('author_name'),
+            'cover_image': book.get('pic_200'),
+            'url': book.get('share_url').get('share_url'),
+            'reviews': book.get('count_reviews', 0),
+        })
     return results
--- books/urls.py
+++ books/urls.py
@@ -5,18 +5,19 @@ urlpatterns = [
     path('', catalog.index, name='index'),
     path('catalog', catalog.catalog, name='catalog-all-books'),
     path('catalog/<slug:tag_slug>', catalog.catalog, name='catalog-for-tag'),
-
     path('books/<slug:slug>', book.book_detail, name='book-detail-page'),
-    path('person/<slug:slug>', person.person_detail, name='person-detail-page'),
-    path('publisher/<slug:slug>', publisher.publisher_detail, name='publisher-detail-page'),
-
+    path('person/<slug:slug>', person.person_detail,
+         name='person-detail-page'),
+    path('publisher/<slug:slug>',
+         publisher.publisher_detail,
+         name='publisher-detail-page'),
     path('about', stats.about, name='about'),
     path('stats/birthdays', stats.birthdays),
     path('stats/digest', stats.digest),
-
     path('articles', articles.redirect_to_first_article, name='all-articles'),
-    path('articles/<slug:slug>', articles.single_article, name='single-article'),
-
+    path('articles/<slug:slug>',
+         articles.single_article,
+         name='single-article'),
     path('search', support.search, name='search'),
     path('push_data_to_algolia', support.push_data_to_algolia),
     path("404", support.page_not_found),
@@ -25,6 +26,6 @@ urlpatterns = [
     path('data.json', support.get_data_json),
     path('generate_data_json', support.generate_data_json),
     path('update_read_by_author_tag', support.update_read_by_author_tag),
-    path('markdown_preview', support.markdown_to_html),
-    path('livelib_books', support.get_livelib_books)
+    path('api/markdown_preview', support.markdown_to_html),
+    path('api/livelib_books', support.get_livelib_books)
 ]

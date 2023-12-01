e591d5b9de9aa36cefee5446f2ab9641c2cc51aa
Mikita Belahlazau <nikelandjelo@gmail.com>
2023-12-01 00:05:56 -0800

    Use narrations instead of books on digest page.

---
+++ tests/test_stats_pages.py
@@ -0,0 +1,19 @@
+
+from tests.webdriver_test_case import WebdriverTestCase
+
+
+class StatsPagesTest(WebdriverTestCase):
+    '''Tests for various robot-related pages like robots.txt, sitemap.'''
+
+    def test_digest_page(self):
+        self.fake_data.create_book_with_single_narration(title='Book')
+        self.fake_data.create_book_with_single_narration(title='Book 2')
+
+        # Just verify that it renders without errors.
+        # TODO: Add more checks.
+        self.driver.get(f'{self.live_server_url}/stats/digest')
+
+    def test_birthdays_page(self):
+        # Just verify that it renders without errors.
+        # TODO: Add more checks.
+        self.driver.get(f'{self.live_server_url}/stats/birthdays')

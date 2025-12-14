647315ea5597b6a04bd7adcc32a720a83f9a415c
Mikita Belahlazau <nikelandjelo@gmail.com>
2025-12-14 15:23:59 +0100

    Fix deployment: toolbar was causing issues.
    RuntimeError: Model class debug_toolbar.models.HistoryEntry doesn't declare an explicit app_label and isn't in an application in INSTALLED_APPS

--- booksby/urls.py
+++ booksby/urls.py
@@ -21,6 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__debug__/", include("debug_toolbar.urls")),
     path("", include("books.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- deploy_site.sh
+++ deploy_site.sh
@@ -1,3 +1,6 @@
 python manage.py collectstatic --no-input
 python manage.py update_translations
 gcloud app deploy --verbosity=info --no-promote
+
+echo "To update deployed version:"
+echo "https://console.cloud.google.com/appengine/versions?serviceId=default&project=audiobooksbysite"
